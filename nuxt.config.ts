5f7848b83c1ce43eb4d56ae30e3c349631e88b98
Thomas Sterrenburg <th.sterrenburg@gmail.com>
2022-06-02 00:18:00 +0200

    static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    ssr: false,
+    target: 'static',
     css: ['vuetify/lib/styles/main.sass',
         '@mdi/font/css/materialdesignicons.min.css',],
     build: {
