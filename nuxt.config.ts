42f6456d897de748b0edeae49146530165337728
Thomas Sterrenburg <th.sterrenburg@gmail.com>
2022-06-02 00:18:49 +0200

    title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    meta: {
+        title: 'De Inclusiewijzer'
+    },
     target: 'static',
     css: ['vuetify/lib/styles/main.sass',
         '@mdi/font/css/materialdesignicons.min.css',],
