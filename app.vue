a4a1711718394aac42cfa7976c327f38cd87d693
Thomas Sterrenburg <th.sterrenburg@gmail.com>
2022-06-02 00:11:40 +0200

    qr code

--- app.vue
+++ app.vue
@@ -2,13 +2,14 @@
   <v-app>
     <v-app-bar theme="dark" flat>
       <v-app-bar-title>De Inclusiewijzer</v-app-bar-title>
+      <v-spacer></v-spacer>
+      <code-menu></code-menu>
     </v-app-bar>
     <v-main>
       <v-img src="assets/inclusiewijzer1.jpg" width="100%"></v-img>
-
     </v-main>
-    <v-footer theme="dark"><span class="mx-auto text-caption"><a href="https://www.linkedin.com/in/myrthe-sterrenburg"
-          target="_blank">🌸
+    <v-footer theme="dark"><span class="mx-auto my-4 text-caption"><a
+          href="https://www.linkedin.com/in/myrthe-sterrenburg" target="_blank">🌸
           2022 Myrthe
           Sterrenburg</a></span></v-footer>
   </v-app>
---
+++ components/code-menu.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-menu anchor="bottom start" origin="start" :close-on-content-click="false" transition="slide-y-transition">
+        <template #activator="{ props }">
+            <v-btn icon="mdi-qrcode" title="QR code" class="ml-4" v-bind="props">
+            </v-btn>
+        </template>
+
+        <v-card width="300" class="code">
+            <v-img src="assets/qr-code.png"></v-img>
+        </v-card>
+    </v-menu>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.code {
+    margin-top: 18px;
+    right: 250px;
+}
+</style>
