b16f2157de191ec147dd141492a07494011c4a83
Thomas Sterrenburg <th.sterrenburg@gmail.com>
2022-06-07 09:34:26 +0200

    pdf

--- components/code-menu.vue
+++ components/code-menu.vue
@@ -1,14 +1,15 @@
 <template>
-    <v-menu anchor="bottom start" origin="start" :close-on-content-click="false" transition="slide-y-transition">
+    <!-- <v-menu anchor="bottom start" origin="start" :close-on-content-click="false" transition="slide-y-transition">
         <template #activator="{ props }">
-            <v-btn icon="mdi-qrcode" title="QR code" class="ml-4" v-bind="props">
+            <v-btn icon="mdi-file-pdf-box" title="Bekijk PDF" class="ml-4" v-bind="props">
             </v-btn>
         </template>
 
         <v-card width="300" class="code">
             <v-img src="qr-code.png"></v-img>
         </v-card>
-    </v-menu>
+    </v-menu> -->
+    <v-btn icon="mdi-file-pdf-box" title="Bekijk PDF" href="/de_inclusiewijzer.pdf" target="_blank"></v-btn>
 </template>
 
 <script setup lang="ts">
